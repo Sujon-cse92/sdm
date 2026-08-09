d6cfec9cec890389e602adcfa0b1907736c64f0a
jannatul1732 <jannatulferdousi7777@gmail.com>
2026-08-09 11:35:08 +0600

    add new class

--- Student.java
+++ Student.java
@@ -26,3 +26,31 @@ public class Student1 {
         this.age = age;
     }
 }
+public class Student2 {
+    private String name;
+    private int age;
+
+    public Student2() {
+    }
+
+    public Student2(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
